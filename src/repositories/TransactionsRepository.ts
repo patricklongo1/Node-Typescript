9db15da1ff59fda44f4e197269f3f300bc78b104
patricklongo1 <ck.longo@bol.com.br>
2020-04-14 09:57:23 -0300

    Done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,22 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable func-names */
 import Transaction from '../models/Transaction';
+
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface List {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +26,50 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): List {
+    const incomes = this.transactions.filter(transaction =>
+      transaction.type === 'income' ? transaction : null,
+    );
+
+    const outcomes = this.transactions.filter(transaction =>
+      transaction.type === 'outcome' ? transaction : null,
+    );
+
+    const income = this.getBalance(incomes, 'income');
+    const outcome = this.getBalance(outcomes, 'outcome');
+    return {
+      transactions: this.transactions,
+      balance: {
+        income,
+        outcome,
+        total: income - outcome,
+      },
+    };
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getBalance(tsc: Transaction[], type: string): number {
+    if (type === 'income') {
+      const incomeTotal = tsc.reduce(function (acc, value) {
+        return acc + value.value;
+      }, 0);
+      return incomeTotal;
+    }
+    const outcomeTotal = tsc.reduce(function (acc, value) {
+      return acc + value.value;
+    }, 0);
+    return outcomeTotal;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const transaction = new Transaction({
+      title,
+      type,
+      value,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
